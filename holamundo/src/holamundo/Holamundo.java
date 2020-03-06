b4ff12bcfc89335191054a92230d88a9af3ca36a
Marcos9819 <61875951+Marcos9819@users.noreply.github.com>
2020-03-06 18:27:44 +0000

    Add files via upload

--- holamundo/src/holamundo/Holamundo.java
+++ holamundo/src/holamundo/Holamundo.java
@@ -3,9 +3,16 @@ package holamundo;
 public class Holamundo {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Hola mundo");
-        
+        adiosmundo();
+
     }
-    
+
+    public static void adiosmundo() {
+
+        System.out.println("Adios mundo");
+
+    }
+
 }
